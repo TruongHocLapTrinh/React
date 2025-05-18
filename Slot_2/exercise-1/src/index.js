912776fa7aab91ac8de2884d9b3541857035d3b0
Truong <lenhattruong295@gmail.com>
2025-05-18 23:17:20 +0700

    exercise 6-10

--- Slot_2/exercise-1/src/index.js
+++ Slot_2/exercise-1/src/index.js
@@ -5,13 +5,41 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import NamePerson from "./NamePerson";
 import PeopleList from './PeopleList';
+import FirstTeenager from './FirstTeenager';
+import AreAllTeenagers from './AreAllTeenagers';
+import SortByOccupationAndAge from './SortByOccupationAndAge';
+import GroupByOccupation from './GroupByOccupation';
+import OldestAndYoungest from './OldestAndYoungest';
+import AverageAgeByOccupation from './AverageAgeByOccupation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <NamePerson />
-    <PeopleList/>
+    {/* <App /> */}
+
+    {/* Exer 2 */}
+    {/* <NamePerson /> */}
+
+    {/* Exer 3 4 5 */}
+    {/* <PeopleList/> */}
+
+    {/* Exer 6 */}
+    <FirstTeenager/>
+
+    {/* Exer 7 */}
+    <AreAllTeenagers/>
+
+    {/* Exer 8 */}
+    <SortByOccupationAndAge/>
+
+    {/* Exer 9 */}
+    <GroupByOccupation/>
+
+    {/* Exer 10 */}
+    <OldestAndYoungest/>
+
+    {/* Exer 11 */}
+    <AverageAgeByOccupation/>
   </React.StrictMode>
 );
 
